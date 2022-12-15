19184c9b5e4c53d04df49eeff868bf59f62dcdbc
Adrian Rutkowski <adrianrutkowskiarcgas@gmail.com>
2022-12-15 21:47:04 +0100

    Wstep

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,5 +1,7 @@
 package pl.javastart.task;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +12,27 @@ public class Main {
     }
 
     public void run(Scanner scanner) {
-        // uzupełnij rozwiązanie. Korzystaj z przekazanego w parametrze scannera
+        String dataFormat = "yyyy-MM-dd HH:mm:ss";
+        String tempHour = " 00:00:00";
+        DateTimeFormatter datePattern = DateTimeFormatter.ofPattern(dataFormat);
+        LocalDateTime localDateTime;
+        System.out.println("Podaj datę");
+        String dataInput = scanner.nextLine();
+        if (dataInput.length() <= 10) {
+            localDateTime = LocalDateTime.parse(dataInput + tempHour, datePattern);
+            System.out.println(localDateTime.format(datePattern));
+        } else {
+            localDateTime = LocalDateTime.parse(dataInput, datePattern);
+        }
+        ZonedDateTime sydney = localDateTime.atZone(ZoneId.of("Australia/Sydney"));
+        System.out.println("Czas lokalny: " + localDateTime.atZone(ZoneId.systemDefault()).format(datePattern));
+        System.out.println("UTC: " + localDateTime.atZone(ZoneId.of("UTC")).format(datePattern));
+        System.out.println("Londyn: " + localDateTime.atZone(ZoneId.of("Europe/London")).format(datePattern));
+        System.out.println("Los Angeles: " + localDateTime.atZone(ZoneId.of("America/Los_Angeles")).format(datePattern));
+        System.out.println("Sydney: " + sydney.format(datePattern));
+
     }
 
 }
+
+
